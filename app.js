99ef1a3feb02c640c681b1343a0aabcdaa5d88f4
Catrina Alex <ovidiualex.catrina@gmail.com>
2022-01-14 23:36:43 +0200

    other commit

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 //const apiKEY =  '04c35731a5ee918f014970082a0088b1';
 const apiURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const imgPATH="https://image.tmdb.org/t/p//w1280";
+const main=document.querySelector('main');
+
 async function getMovies(){
     const resp=await fetch(apiURL);
     const respData=await resp.json();
     console.log(respData);
-
-
 
    /* respData.results.forEach((movie) => {
 
@@ -17,6 +17,26 @@ async function getMovies(){
         document.body.appendChild(img);
         
     });*/
+
+    respData.results.forEach((movie) => {
+
+        const {poster_path,title,vote_average}=movie;
+        let movieElement=document.createElement("div");
+        movieElement.classList.add("movie");
+        movieElement.innerHTML=`
+        <img src="${imgPATH+poster_path}" 
+        alt="${title}"/>
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span>${vote_average}</span>
+        </div>
+        `;
+
+        main.appendChild(movieElement);
+        
+    });
+
+
     return respData;
 } 
 
