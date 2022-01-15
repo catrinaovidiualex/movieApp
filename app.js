8a7fc5a8f3c077524a9c766ad6e840724baa64cc
Catrina Alex <ovidiualex.catrina@gmail.com>
2022-01-15 13:34:24 +0200

    other commit

--- app.js
+++ app.js
@@ -2,17 +2,20 @@
 //const apiKEY =  '04c35731a5ee918f014970082a0088b1';
 const apiURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const imgPATH="https://image.tmdb.org/t/p//w1280";
-let main=document.querySelector('main');
-let form=document.querySelector('form');
-let search=document.querySelector('search');
+let searchAPI="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+let main=document.getElementById("main");
+let form=document.getElementById("form");
+let search=document.getElementsByClassName("search");
 
-getMovies();
+getMovies(apiURL);
 
 
-async function getMovies(){
-    const resp=await fetch(apiURL);
+
+
+async function getMovies(url){
+    const resp=await fetch(url);
     const respData=await resp.json();
-    console.log(respData);
+    /*console.log(respData);*/
 
    /* respData.results.forEach((movie) => {
 
@@ -21,7 +24,7 @@ async function getMovies(){
 
         document.body.appendChild(img);
         
-    });*/
+    });
 
     respData.results.forEach((movie) => {
 
@@ -39,15 +42,52 @@ async function getMovies(){
 
         main.appendChild(movieElement);
         
+    });*/
+    showMovies(respData.results);
+
+   
+} 
+
+
+function showMovies(movies){
+   
+    // golim main-ul
+    main.innerHTML='';
+    movies.forEach((movie) => {
+
+        const {poster_path,title,vote_average}=movie;
+        let movieElement=document.createElement("div");
+        movieElement.classList.add("movie");
+        movieElement.innerHTML=`
+        <img src="${imgPATH+poster_path}" 
+        alt="${title}"/>
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span>${vote_average}</span>
+        </div>
+        `;
+
+        main.appendChild(movieElement);
     });
 
+ 
+}
 
-    return respData;
-} 
+
 
 form.addEventListener('submit',(e) =>{
 e.preventDefault();
 let searchTerm=search.value;
 
+if(searchTerm){
 
-})
+  getMovies(searchAPI+searchTerm);
+
+    search.value='';
+
+}
+
+
+
+
+});
